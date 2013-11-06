27cdd467a8ea99d6be012e12a77821541b2959da
ShinMunkyu <skylershin@occupie.kr>
2013-11-06 15:59:16 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ExportExcel::Application.config.secret_key_base = 'bbc2bdc0a731bdce227698f41b5f879f2d23faad7ebeda28325d0bc2c3b7c24f23de6cfc0d4e72dd49922784803495e7dc8d3539a5f80e30b9284cbf09273a5d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ExportExcel::Application.config.session_store :cookie_store, key: '_exportExcel_session'
